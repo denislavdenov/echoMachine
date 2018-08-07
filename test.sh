01aca293f127e5cc1c428cacf837518a89a9679e
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2018-08-07 16:08:20 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,10 +6,18 @@ if [ $? -ne 0 ]; then
   echo something went wrong
 fi
 
-if [ "${OUTPUT}" == "atanas" ]; then
+# test 1 make sure arg is in output
+if [[ "${OUTPUT}" =~ "atanas" ]]; then
   echo all good!
 else
-  echo "err: expected atanas got ${OUTPUT}"
+  echo "err: expected string with atanas but got ${OUTPUT}"
   exit 1
 fi
 
+# test 2 make sure output is nice
+if [[ "${OUTPUT}" =~ "nice" ]]; then
+  echo all good!
+else
+  echo "err: expected string with nice but got ${OUTPUT}"
+  exit 1
+fi
