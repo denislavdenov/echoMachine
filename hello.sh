66eb5530d07f1cb09f4f8af4a37c1fa147803881
Alvaro Miranda Aguilera <kikitux@gmail.com>
2018-08-08 13:11:47 +0300

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,12 +1,3 @@
 #!/usr/bin/env bash
 
-# implement the function
-function hello(){
-  echo "Hello ${1}, welcome to the echoMachinator! Have a nice day!"
-}
-
-# use the function
-user=${1}
-
-# hello user
-hello ${user}
+exit 0
